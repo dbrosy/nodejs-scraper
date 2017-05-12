cc3845e875fee3fde4ddf362903d0d9d31fd5215
dbrosy <dbrosy@hotmail.com>
2017-05-12 18:10:05 +1000

    Update server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,15 +1,10 @@
 var noodle = require('noodlejs');
 
-app.get('/:text', function(req, res) {
-  noodle.query({ url: 'http://google.com/search?q='+req.params.text,
-
-  selector: 'h3.r a',            // If no `selector` is specified than the entire document is returned. This is a 
-                                // rule applied to all types of docments. The `extract` rule will be ignored if included.
-  extract:  ["href", "text"]    // "text" if you want to display only the text
-                            // "href" if you want to display only the links
-                            // In case multiple extracts give it as an array ie ["href", "text"]
+noodle.query({
+  url:      'https://github.com/explore',
+  selector: 'ol.ranked-repositories h3 a',
+  extract:  'href'
 })
 .then(function (results) {
-  res.send(results);             // return the result
-});
+  console.log(results);
 });
