9dc071a9bc388a0136871c011bf419ad5cca84d4
dbrosy <dbrosy@hotmail.com>
2017-05-12 14:14:01 +1000

    Update server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,15 +1,30 @@
 'use strict';
 
 const express = require('express');
+const noodle = require('noodlejs');
+
 
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
+
+var query = {
+    url: 'http://google.com/search?q=javascript',
+    type: 'html',
+    selector: 'h3.r a',
+    extract: 'text'
+  },
+  uriQuery = encodeURIComponent(JSON.stringify(query)),
+  request  = 'http://example.noodlejs.com/?q=' +
+             uriQuery + '&callback=?';
+
+// Make Ajax request to Noodle server
+jQuery.getJSON(request, function (data) {
+  alert(data[0].results);
 });
+
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
