7149374d81ccf67d73c1145bb93ebd44369874cc
dbrosy <dbrosy@hotmail.com>
2017-05-12 18:07:11 +1000

    Update server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,41 +1,15 @@
-'use strict';
+var noodle = require('noodlejs');
 
-const express = require('express');
-const noodle = require('noodlejs');
+app.get('/:text', function(req, res) {
+  noodle.query({ url: 'http://google.com/search?q='+req.params.text,
 
-
-// Constants
-const PORT = 8080;
-
-// App
-const app = express();
-/*
-var jsdom = require('jsdom').jsdom
-  , myWindow = jsdom().createWindow()
-  , $ = require('jQuery')
-  , jq = require('jQuery').create()
-  , jquery = require('jquery').create(myWindow)
-  ;
-*/
-
-var jquery = require('jquery');
-
-var query = {
-    url: 'http://google.com/search?q=javascript',
-    type: 'html',
-    selector: 'h3.r a',
-    extract: 'text'
-  },
-  uriQuery = encodeURIComponent(JSON.stringify(query)),
-  request  = 'http://example.noodlejs.com/?q=' +
-             uriQuery + '&callback=?';
-
-// Make Ajax request to Noodle server
-//var js = jquery.Create(myWindow);
-jquery.getJSON(request, function (data) {
-  alert(data[0].results);
+  selector: 'h3.r a',            // If no `selector` is specified than the entire document is returned. This is a 
+                                // rule applied to all types of docments. The `extract` rule will be ignored if included.
+  extract:  ["href", "text"]    // "text" if you want to display only the text
+                            // "href" if you want to display only the links
+                            // In case multiple extracts give it as an array ie ["href", "text"]
+})
+.then(function (results) {
+  res.send(results);             // return the result
 });
-
-
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+});
